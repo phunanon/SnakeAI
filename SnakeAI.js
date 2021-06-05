c9ba7a166a19b0f40d888fd55c3f7998629b4ffc
phunanon <phunanon@users.noreply.github.com>
2021-06-05 18:27:55 +0100

    Remove brain preview; misc

--- SnakeAI.js
+++ SnakeAI.js
@@ -14,43 +14,26 @@ const mutant = (brain, rn, rate = .2) => brain.map(l => l.map(({ bias, weights }
     bias: rn() < rate ? rn() : bias,
     weights: weights.map(w => (rn() < rate ? rn() * 2 - 1 : w)),
 })));
-function draw({ brain, body, head, food, ate, age }, message, board, info) {
+function draw({ body, head, food, ate, age }, message, board) {
     const [height, width] = [body.length, body[0].length];
     board.clearRect(0, 0, board.canvas.width, board.canvas.height);
     board.save();
     board.scale(board.canvas.width / width, board.canvas.height / height);
     //Draw Snake body & head
-    board.fillStyle = "#000";
-    body.forEach((row, y) => row.forEach((dot, x) => dot && board.fillRect(x, y, 1, 1)));
+    body.forEach((row, y) => row.forEach((dot, x) => {
+        board.fillStyle = `rgb(0, ${(dot / (ate + 2)) * 200}, 0)`;
+        dot && board.fillRect(x, y, 1, 1);
+    }));
+    board.fillStyle = "rgb(0, 200, 0)";
     board.fillRect(head.x, head.y, 1, 1);
     //Draw food
     board.fillStyle = "#d00";
     board.fillRect(food.x, food.y, 1, 1);
     //Draw info
-    info.clearRect(0, 0, info.canvas.width, info.canvas.height);
-    info.fillStyle = "#000";
-    info.font = "12px Arial";
-    info.fillText(`ate ${ate}, age ${age}, ${message}`, 12, 24);
-    drawBrain(brain, info);
+    board.fillStyle = "#000";
+    board.font = ".5px Arial";
+    board.fillText(`ate ${ate}, age ${age}, ${message}`, 0.5, 1);
     board.restore();
-}
-function drawBrain(brain, info) {
-    const margin = 1.2;
-    info.save();
-    info.translate(32, 48);
-    info.scale(24, 24);
-    brain.forEach((l, x) => l.forEach((n, y) => {
-        //Bias
-        const B = n.bias * 255;
-        info.fillStyle = `rgb(${255 - B}, ${255 - B}, ${255 - B})`;
-        info.fillRect(x * margin - .1, y * margin - .1, margin, margin);
-        //Result
-        let r = n.bias;
-        let R = (r < 0 ? r * -255 : 0), G = (r > 0 ? r * 255 : 0);
-        info.fillStyle = `rgb(${255 - G}, ${255 - R}, ${255 - R - G})`;
-        info.fillRect(x * margin, y * margin, 1, 1);
-    }));
-    info.restore();
 }
 const w = 16, h = 16, timeout = (w + h) * 2;
 const birth = (brain) => ({
--- src/Draw.ts
+++ src/Draw.ts
@@ -1,8 +1,7 @@
 function draw(
-    { brain, body, head, food, ate, age }: LiveSnake,
+    { body, head, food, ate, age }: LiveSnake,
     message: string,
     board: CanvasRenderingContext2D,
-    info: CanvasRenderingContext2D,
 ) {
     const [height, width] = [body.length, body[0].length];
     board.clearRect(0, 0, board.canvas.width, board.canvas.height);
@@ -10,8 +9,13 @@ function draw(
     board.scale(board.canvas.width / width, board.canvas.height / height);
 
     //Draw Snake body & head
-    board.fillStyle = "#000";
-    body.forEach((row, y) => row.forEach((dot, x) => dot && board.fillRect(x, y, 1, 1)));
+    body.forEach((row, y) =>
+        row.forEach((dot, x) => {
+            board.fillStyle = `rgb(0, ${(dot / (ate + 2)) * 200}, 0)`;
+            dot && board.fillRect(x, y, 1, 1);
+        }),
+    );
+    board.fillStyle = "rgb(0, 200, 0)";
     board.fillRect(head.x, head.y, 1, 1);
 
     //Draw food
@@ -19,28 +23,9 @@ function draw(
     board.fillRect(food.x, food.y, 1, 1);
 
     //Draw info
-    info.clearRect(0, 0, info.canvas.width, info.canvas.height);
-    info.fillStyle = "#000";
-    info.font = "12px Arial";
-    info.fillText(`ate ${ate}, age ${age}, ${message}`, 12, 24);
-
-    drawBrain(brain, info);
+    board.fillStyle = "#000";
+    board.font = ".5px Arial";
+    board.fillText(`ate ${ate}, age ${age}, ${message}`, 0.5, 1);
 
     board.restore();
-}
-
-function drawBrain(brain: Brain, info: CanvasRenderingContext2D) {
-    const margin = 1.2;
-    info.save();
-    info.translate(32, 48);
-    info.scale(24, 24);
-    brain.forEach((l, x) =>
-        l.forEach((n, y) => {
-            let r = n.bias;
-            let R = (r < 0 ? r * -255 : 0), G = (r > 0 ? r * 255 : 0);
-            info.fillStyle = `rgb(${255 - G}, ${255 - R}, ${255 - R - G})`;
-            info.fillRect(x * margin, y * margin, 1, 1);
-        }),
-    );
-    info.restore();
 }
